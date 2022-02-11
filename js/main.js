0e5c10c0dcdcbbdddbe43b4ccee83cafdcc7eba6
OGBeas222 <zholudev111@gmail.com>
2022-02-11 17:15:44 +0300

    finished commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+  $('.about-btn').click(function(){
+    $('.social-links').toggleClass('social-links__active');
+    $('.social-elem').slideToggle(435);
+  });
+
+});
